2fb20324be92dc61c6c88f51d05b28844c10765b
Schweigs <mjschweiger@gmail.com>
2016-03-05 12:09:44 +0000

    Lectures 15: Template Context

--- posts/views.py
+++ posts/views.py
@@ -5,15 +5,32 @@ from django.shortcuts import render
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
+
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    context = {
+        'title': 'Detail'
+    }
+    return render(request, "index.html", context)
+
 
 def post_list(request):
-    return render(request, "index.html")
-    # return HttpResponse("<h1>List</h1>")
+    # if request.user.is_authenticated():
+    #     context = {
+    #         'title': 'My user list'
+    #     }
+    # else:
+    #     context = {
+    #         'title': 'List'
+    #     }
+    context = {
+             'title': 'List'
+         }
+    return render(request, "index.html", context)
+
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
+
 
 def post_delete(request):
     return HttpResponse("<h1>Delete</h1>")

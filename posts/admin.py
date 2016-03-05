4f1608aa636b04784643a4898d38b99d14974fb0
Schweigs <Gith-123>
2016-03-05 10:57:51 +0000

    Lecture 8: Customize Admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from .models import Post
 
-admin.site.register(Post)
+
+# Register your models here.
+
+# Documentation: https://docs.djangoproject.com/en/1.9/ref/contrib/admin/
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ("title", "timestamp", "updated")
+    list_display_links = ["timestamp"]
+    list_editable = ["title"]
+    list_filter = ['title', 'updated']
+    search_fields = ['title', 'content']
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostModelAdmin)

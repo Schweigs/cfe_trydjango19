260788ed0cb8882a37a469e9612cb4dd75001079
Schweigs <mjschweiger@gmail.com>
2016-03-05 13:00:22 +0000

    Lectures 19: URL links & Get Absolute URL

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 # MVC - Model View Controller
@@ -12,7 +13,9 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
-
-
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('posts:detail', kwargs={'id': self.id})
+        # return "/posts/%s/" %(self.id)

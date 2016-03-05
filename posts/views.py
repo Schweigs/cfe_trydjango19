31c1fd582982f6b08c1a6b43abf9c5f370c3fd0e
Schweigs <mjschweiger@gmail.com>
2016-03-05 12:31:25 +0000

    Lectures 17: Get Item or 404 Query

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -8,10 +8,15 @@ def post_create(request):
 
 
 def post_detail(request):
+    # this method could through an error if the id doesn't exist:
+    # instance = Post.objects.get(id=1)
+    # this method will return a 404 page instead:
+    instance = get_object_or_404(Post, id=4)
     context = {
-        'title': 'Detail'
+        'title': instance.title,
+        'instance': instance,
     }
-    return render(request, "index.html", context)
+    return render(request, "post_detail.html", context)
 
 
 def post_list(request):

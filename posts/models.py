4ddb7505a4009b972753c4450eb1c0d12f2ffa34
Schweigs <Gith-123>
2016-03-09 18:11:07 +0000

    Lecture 28: Slug Fields

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import pre_save
+
+from django.utils.text import slugify
 
 # Create your models here.
 # MVC - Model View Controller
@@ -9,6 +12,7 @@ from django.core.urlresolvers import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
@@ -22,3 +26,23 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-timestamp", "-updated"]
+
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by('-id')
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_safe_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+
+pre_save.connect(pre_safe_post_receiver, sender=Post)
